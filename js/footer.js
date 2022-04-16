d3b4f10f7a8af911a4484c23d740d2b6f4273776
plexion <46572320+plexiondev@users.noreply.github.com>
2022-04-16 02:20:53 +0100

    fix footer

---
+++ js/footer.js
@@ -0,0 +1,19 @@
+// load footer
+
+
+document.getElementById('footer').innerHTML =
+(`
+<div class="footer-inner">
+<span class="main">
+    <p>plexion 2022.</p>
+</span>
+<span class="links">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/library">Library</a>
+    <a href="/contact">Contact</a>
+    <a href="https://github.com/plexiondev/grand-scheme-of-things">View Source</a>
+    <a href="/accessibility">Accessibility</a>
+</span>
+</div>
+`);
